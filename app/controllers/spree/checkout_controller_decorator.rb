d66daa0694cf71cae0f13c687376ee9bfd9f85fa
Víctor Martín <ilovemayalopez@gmail.com>
2013-05-21 18:54:07 +0200

    Avoid bug when guest checkout

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -16,7 +16,7 @@ module Spree
         .request(
           :merchant_id            => gateway.preferred_merchant_id,
           :amount                 => (@order.total.to_f*100).to_i,
-          :customer_id            => current_user.id,
+          :customer_id            => (current_user ? current_user.id : 0),
           :order_id               => @order.id,
           :automatic_response_url => "#{base_url}/atos/atos_auto_response",
           :normal_return_url      => "#{base_url}/atos/atos_confirm",
