414dc55b83963ab96a47ef1edf77680358cb176f
David Demonchy - Fusco <fusco.spv@gmail.com>
2013-06-23 02:10:30 +0200

    Move Atos information

--- app/models/spree/atos_sips_account.rb
+++ app/models/spree/atos_sips_account.rb
@@ -1,5 +1,14 @@
 class Spree::AtosSipsAccount < ActiveRecord::Base
-  attr_accessible :payment_means, :card_number, :customer_id
+  attr_accessible :payment_means,
+                  :card_number,
+                  :customer_id,
+                  :response_code,
+                  :avs_response,
+                  :transaction_id,
+                  :authorisation_id
+
 
   has_many :payments, :as => :source
+
+
 end
